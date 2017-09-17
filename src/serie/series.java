32e5197d955d478ab0c12b3a5f12285af4b5f676
manvika123 <manvikajaiswal1995@gmail.com>
2017-09-17 13:08:29 +0530

    first commit

---
+++ src/serie/series.java
@@ -0,0 +1,15 @@
+package serie;
+
+public class series {
+
+    public static void main(String[] args) {
+
+        int i,sum=0;
+
+        for(i=1;i<=9;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
